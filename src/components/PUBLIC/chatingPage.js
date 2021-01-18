0df95c5a0a2465ba04bc16ba86aefb29067279cf
jiwon <jidole041214@naver.com>
2021-01-18 17:54:55 +0900

    다크모드 구현

--- src/components/PUBLIC/chatingPage.js
+++ src/components/PUBLIC/chatingPage.js
@@ -3,36 +3,37 @@ import * as s from '../STYLECOMPONENT/publicStyle'
 import * as c from '../STYLECOMPONENT/chatingStyle'
 
 
-function ChatingPage(){
-    const [Img, setImg] = useState('');
-    function fileUpload(e){
-        console.log(e.target.files[0].name)
-        setImg(e.target.files[0].name)
+function ChatingPage(Lang){
+    const a = Lang.Lang;
+    const [mode, setMode] = useState();
+    const DarkMode=()=>{
+        setMode('dark')
+    }
+    const LightMode=()=>{
+        setMode('light')
     }
     return(
-        <c.ChatingBox>
-            <c.SideBar>
+        <c.ChatingBox style={{backgroundColor:(mode=='dark')?'rgb(70,70,70)':''}}>
+            <c.SideBar style={{backgroundColor:(mode=='dark')?'rgb(50,50,50)':''}}>
                 <c.SettingChat>
                     <c.SettingMenu>
-                    <i class="fas fa-cloud-moon"></i> 색상 테마
+                    <i class="fas fa-cloud-moon"></i> {(a==0)?"COLOR THEME" : "색상 테마"}
                     </c.SettingMenu>
-                    <c.SettingChoose><i class="fas fa-moon"></i>NIGHT</c.SettingChoose>
-                    <c.SettingChoose><i class="fas fa-sun"></i>MORNIG</c.SettingChoose>
+                    <c.SettingChoose style={{backgroundColor:(mode=='dark')?'rgb(100,100,100)':''}} onClick={DarkMode}><i class="fas fa-moon"></i>{(a==0)?"DARK MODE" : "어두운 테마"}</c.SettingChoose>
+                    <c.SettingChoose style={{backgroundColor:(mode=='dark')?'rgb(100,100,100)':''}} onClick={LightMode}><i class="fas fa-sun"></i>{(a==0)?"LIGHT MODE" : "밝은 테마"}</c.SettingChoose>
                     <c.SettingMenu>
-                    <i class="far fa-images"></i> 배경 설정
+                    <i class="fas fa-cog"></i>{(a==0)?"SETTING" : "설정"}
                     </c.SettingMenu>
-                    <c.UploadBox>
-                        <input type="file" onChange={fileUpload}/>
-                    </c.UploadBox>
-                    <img src={Img} alt=""/>
+                    <c.SettingChoose style={{backgroundColor:(mode=='dark')?'rgb(100,100,100)':''}}><i class="fas fa-exclamation"></i>{(a==0)?"REPORT" : "신고하기"}</c.SettingChoose>
+                    <c.SettingChoose style={{backgroundColor:(mode=='dark')?'rgb(100,100,100)':''}}><i class="fas fa-sign-out-alt"></i>{(a==0)?"EXIT" : "나가기"}</c.SettingChoose>
                 </c.SettingChat>
             </c.SideBar>
             <c.ChatingContainer></c.ChatingContainer>
-            <c.RightSideBar></c.RightSideBar>
-            <c.UnderBar>
+{/*             <c.RightSideBar></c.RightSideBar> */}
+            <c.UnderBar style={{backgroundColor:(mode=='dark')?'rgb(50,50,50)':''}}>
                 <c.InputChatBox>
-                    <c.InputChat placeholder="보낼 내용을 입력하세요!"></c.InputChat>
-                    <c.SendChatBtn><i class="fas fa-paper-plane"></i></c.SendChatBtn>
+                    <c.InputChat style={{backgroundColor:(mode=='dark')?'rgb(100,100,100)':'',  color:(mode=='dark')?'whitesmoke':'',border:(mode=='dark')?'none':''}} placeholder="보낼 내용을 입력하세요!"></c.InputChat>
+                    <c.SendChatBtn style={{border:(mode=='dark')?'none':''}}><i class="fas fa-paper-plane"></i></c.SendChatBtn>
                 </c.InputChatBox>
             </c.UnderBar>
         </c.ChatingBox>
