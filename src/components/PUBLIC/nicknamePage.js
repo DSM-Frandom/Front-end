ddb0587f0f149220d24e939c613feeed9e584db3
jiwon <jidole041214@naver.com>
2021-01-23 21:49:45 +0900

    UPDATE : Login FUNC complete

--- src/components/PUBLIC/nicknamePage.js
+++ src/components/PUBLIC/nicknamePage.js
@@ -1,21 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom'
 import * as f from '../STYLECOMPONENT/publicStyle'
-import Loading from './loading'
+import {Request} from '../axios'
 
 function NicknamePage(Lang){
     const a = Lang.Lang;
-    const [toggle, setToggle] = useState();
     const LinkStyle = {position:"relative", width:"100%", height:"100%"}
-    const changeToggle =()=>{setToggle(1);}
+    const [data, setData] = useState({
+        id : "",
+        password: ""
+    });
+    const Input=(e)=>{
+        const {value, name} = e.target;
+        setData({
+            ...data,
+            [name] : value
+        })
+    }
+    const SubInputValue =()=>{
+        Request("POST", "v1/auth/login",{"Content-type":"application/json"}, 
+        {
+            "email" : data.id,
+            "password" : data.password
+        })
+    }
     return(
         <f.FirstPageContainer>
-            {(toggle==1)&&<Loading></Loading>}
             <h1> FRANDOM!</h1>
-            <f.NicknameInput placeholder={(a==0)?"NICKNAME" : "아이디를 입력하세요."}></f.NicknameInput>
-            <f.NicknameInput placeholder={(a==0)?"NICKNAME" : "비밀번호를 입력하세요."}></f.NicknameInput>
+            <f.NicknameInput onChange={Input} name="id" value={data.id} placeholder={(a==0)?"NICKNAME" : "아이디를 입력하세요."}></f.NicknameInput>
+            <f.NicknameInput onChange={Input} name="password" value={data.password} placeholder={(a==0)?"NICKNAME" : "비밀번호를 입력하세요."}></f.NicknameInput>
             <f.ButtonContainer2>
-            <Link style={LinkStyle} to="/chating"><f.NickSubButton onClick={changeToggle}>{(a==0)?"SUBMIT" : "제출하기"}</f.NickSubButton></Link>
+            <Link style={LinkStyle} to="/chating"><f.NickSubButton onClick={SubInputValue}>{(a==0)?"SUBMIT" : "제출하기"}</f.NickSubButton></Link>
             </f.ButtonContainer2>
             <Link to="/signup"><h3>계정이 없으신가요?</h3></Link>
         </f.FirstPageContainer>
