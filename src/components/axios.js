b95897f8e438e7f73d917d98044ad0ccb41dcc3c
jiwon <jidole041214@naver.com>
2021-01-23 20:26:02 +0900

    FIX : BASE URL

--- src/components/axios.js
+++ src/components/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const Request=(method, url, head, data)=>{
     return axios({
         method: method,
-        url: "http://18.221.125.16:3000/" + url,
+        url: "http://3.12.85.2:3000/" + url,
         headers: head,
         data: data
     }).then((e)=>{
