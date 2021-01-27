79899de2d63b77381935415560151dc4d423b117
jiwon <jidole041214@naver.com>
2021-01-27 09:32:32 +0900

    CREATE " Match UI complete / socket

---
+++ src/components/MATCH/match.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import * as s from './styles'
+import { io } from "socket.io-client";
+import {useEffect} from 'react' 
+import {Link} from 'react-router-dom'
+
+const Match=()=>{
+    const LinkStyle = { position:"absolute", width:"13%", height:"6%", marginBottom : "37%"}
+    return(
+        <s.Background>
+            <s.Title>FRANDOM!</s.Title>
+            <Link style={LinkStyle} to="/chating"><s.MatchButton>랜덤채팅 시작!</s.MatchButton></Link>
+        </s.Background>
+    )
+}
+
+export default Match;
---
+++ src/components/MATCH/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const Background = styled.div`
+width:100%;
+height:100%;
+display:flex;
+justify-content:center;
+align-items:center;
+flex-direction:column;
+`
+
+export const Title = styled.h1`
+color:white;
+font-size:7vmin;
+font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+text-shadow:0px 3px 3px rgb(20,20,20,0.2);   
+margin-bottom:0.7%;
+`
+
+export const MatchButton = styled.button`
+width:100%;
+height:100%;
+background-color:rgb(50, 52, 58);
+margin-top:1%;
+border:none;
+color:white;
+font-size:2vmin;
+font-weight:bold;
+`
