cfa4016e83eb4825a8db774a6380eded3423bf86
jiwon <jidole041214@naver.com>
2021-02-15 22:00:46 +0900

    FIX : 토큰 초기화 시켜주기

--- src/components/FIRSTPAGE/firstPage.js
+++ src/components/FIRSTPAGE/firstPage.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as f from '../STYLECOMPONENT/publicStyle'
-import {Link, BrowserRouter} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 function FirstPage(Lang){
     const a = Lang.Lang;
     const LinkStyle = {position:"relative", width:"100%", height:"100%"}
+    useEffect(()=>{
+        window.localStorage.setItem("token"," ")
+    })
     return(
         <f.FirstPageContainer>
             <h1> FRANDOM!</h1>
-            <h3>{(a==0)?"Enjoy random chat with foreigners!" : "외국인과의 랜덤채팅을 즐겨보세요!"}</h3>
+            <h3>{(a===0)?"Enjoy random chat" : "랜덤채팅을 즐겨보세요!"}</h3>
             <f.ButtonContainer>
-                <Link style={LinkStyle} to="/nickname"><f.StartButton>{(a==0)?"GET START" : "시작하기"}</f.StartButton></Link> 
+                <Link style={LinkStyle} to="/nickname"><f.StartButton>{(a===0)?"GET START" : "시작하기"}</f.StartButton></Link> 
             </f.ButtonContainer>
         </f.FirstPageContainer>
     )
